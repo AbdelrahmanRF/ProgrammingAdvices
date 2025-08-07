528d6f33d356741e9bece11edacadcecaca9cb96
AbdelrahmanRF <Abdalrahmanalfar22@gmail.com>
2025-08-07 16:10:58 +0300

    course 10 Date Library Project

---
+++ Course 10/Date Library Project/Date Library Project.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "clsDate.h"
+using namespace std;
+
+int main()
+{
+    clsDate Date1;
+    Date1.Print();
+
+    clsDate Date2("21/8/2025");
+    Date2.Print();
+
+    clsDate Date3(5, 5, 2025);
+    Date3.Print();
+
+    clsDate Date4(250, 2025);
+    Date4.Print();
+
+
+}
+
