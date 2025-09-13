39f0f0246ca012544e32db7c73dc8ea46f9a7e4d
AbdelrahmanRF <Abdalrahmanalfar22@gmail.com>
2025-09-13 10:12:58 +0300

    course 13 project 1

---
+++ Course 13/Project 1/Project 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+using namespace std;
+
+int main()
+{
+    clsDblLinkedList <int> MyDblLinkedList;
+
+    MyDblLinkedList.InsertAtBeginning(5);
+    MyDblLinkedList.InsertAtBeginning(4);
+    MyDblLinkedList.InsertAtBeginning(3);
+    MyDblLinkedList.InsertAtBeginning(2);
+    MyDblLinkedList.InsertAtBeginning(1);
+
+    cout << "\nLinked List Content:\n";
+    MyDblLinkedList.PrintList();
+
+    MyDblLinkedList.InsertAfter(3, 500);
+
+    cout  << "\nAfter Insert: \n";
+    MyDblLinkedList.PrintList();
+
+
+    system("pause>0");
+
+}
